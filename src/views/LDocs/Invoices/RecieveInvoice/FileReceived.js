f883714fd7a1b116472a9b46cc3c63f306aee42b
jehanxaibahmed <jehanxaibahmed@gmail.com>
2021-04-12 15:31:09 +0500

    Chnages

--- src/views/LDocs/Invoices/RecieveInvoice/FileReceived.js
+++ src/views/LDocs/Invoices/RecieveInvoice/FileReceived.js
@@ -217,7 +217,7 @@ export default function FileReceived(props) {
                               Choose Status
                             </MenuItem>
                             <MenuItem value="read">
-                            MARK AS RECEIVED&nbsp;&nbsp;
+                            {fileData.workFlowStatus == "correctionRequired" ? 'RE-SUBMIT' : 'MARK AS RECEIVED'}&nbsp;&nbsp;
                               <div className="fileinput text-center">
                                 <div className="thumbnail img-circle3">
                                   <img src={Success} alt={"RESUBMIT"} />
