d24d31f82ce4bfc0c8d262ab1d92559bae9ca238
jehanxaibahmed <jehanxaibahmed@gmail.com>
2021-10-13 14:18:38 +0500

    Changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux";
 import { PersistGate } from 'redux-persist/integration/react'
-import addNotification from 'react-push-notification';
+import addNotification from 'react-push-notification'; 
 
 
 // import AuthLayout from "layouts/Auth.js";
